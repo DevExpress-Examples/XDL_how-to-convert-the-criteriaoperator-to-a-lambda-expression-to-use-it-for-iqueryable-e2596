a71644243bf5675ddba0be1ab0fb9aba7482a396
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication152/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace DXSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.unitOfWork1 = new DevExpress.Xpo.UnitOfWork();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.filterControl1 = new DevExpress.XtraEditors.FilterControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(537, 478);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(0, 0);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 34);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.OnSaveButtonClick);
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.simpleButton1);
+            this.panelControl1.Controls.Add(this.btnSave);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(0, 478);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(816, 34);
+            this.panelControl1.TabIndex = 2;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(537, 0);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(100, 34);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "ApplyFilter";
+            this.simpleButton1.Click += new System.EventHandler(this.OnUpdateFilter);
+            // 
+            // filterControl1
+            // 
+            this.filterControl1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.filterControl1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.filterControl1.Location = new System.Drawing.Point(537, 0);
+            this.filterControl1.Name = "filterControl1";
+            this.filterControl1.Size = new System.Drawing.Size(279, 478);
+            this.filterControl1.TabIndex = 3;
+            this.filterControl1.Text = "filterControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 512);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.filterControl1);
+            this.Controls.Add(this.panelControl1);
+            this.Name = "Form1";
+            this.Text = "Get Lambda Expression From CriteriaOperator";
+            this.Load += new System.EventHandler(this.OnLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.Xpo.UnitOfWork unitOfWork1;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.FilterControl filterControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+
+    }
+}
+
---
+++ VB/WindowsFormsApplication152/Program.vb
@@ -0,0 +1,81 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows.Forms
+Imports DevExpress.Xpo
+Imports DevExpress.Xpo.DB
+Imports DevExpress.Skins
+
+Namespace DXSample
+	Friend NotInheritable Class Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		Private Sub New()
+		End Sub
+		<STAThread> _
+		Shared Sub Main()
+			Dim connectionString As String = AccessConnectionProvider.GetConnectionString("MyDB.mdb")
+			XpoDefault.DataLayer = XpoDefault.GetDataLayer(connectionString, AutoCreateOption.DatabaseAndSchema)
+			XpoDefault.Session = Nothing
+
+			CreateData()
+
+			SkinManager.EnableFormSkins()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+
+		Private Shared Sub CreateData()
+			Using uow As New UnitOfWork()
+				If uow.FindObject(Of Products)(Nothing) Is Nothing Then
+					Dim product As New Products(uow)
+					product.Discontinued = False
+					product.Price = 100
+					product.Quantity = 20
+					product.ProductName = "Orange"
+
+					product = New Products(uow)
+					product.Discontinued = True
+					product.Price = 140
+					product.Quantity = 50
+					product.ProductName = "Apple"
+
+					product = New Products(uow)
+					product.Discontinued = False
+					product.Price = 30
+					product.Quantity = 120
+					product.ProductName = "Banana"
+
+					product = New Products(uow)
+					product.Discontinued = True
+					product.Price = 200
+					product.Quantity = 80
+					product.ProductName = "Grape"
+
+					product = New Products(uow)
+					product.Discontinued = False
+					product.Price = 300
+					product.Quantity = 10
+					product.ProductName = "Garnet"
+
+					product = New Products(uow)
+					product.Discontinued = True
+					product.Price = 120
+					product.Quantity = 320
+					product.ProductName = "Mandarin"
+
+					product = New Products(uow)
+					product.Discontinued = False
+					product.Price = 250
+					product.Quantity = 60
+					product.ProductName = "Kiwi"
+
+					uow.CommitChanges()
+				End If
+			End Using
+		End Sub
+	End Class
+End Namespace
