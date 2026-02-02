e521c8a2ef5837435668164a99abdecae87ad8dc
DevExpressExampleBot <fakeEmail>
2026-02-02 21:58:13 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApplication152/Form1.vb
+++ VB/WindowsFormsApplication152/Form1.vb
@@ -1,15 +1,10 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
 Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.Xpo
-Imports DevExpress.Data.Filtering
-Imports System.Collections
 Imports DevExpress.XtraEditors
 Imports DevExpress.Data.Linq.Helpers
 Imports DevExpress.XtraEditors.Filtering
@@ -18,52 +13,50 @@ Imports DevExpress.Data.Filtering.Helpers
 Imports DevExpress.Data.Linq
 
 Namespace DXSample
-	Partial Public Class Form1
-		Inherits XtraForm
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private source As IQueryable(Of Products)
-		Private Sub OnLoad(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			Dim products = New XPQuery(Of Products)(unitOfWork1)
+    Public Partial Class Form1
+        Inherits XtraForm
 
-			source = _
-				From product In products _
-				Where product.Quantity >= 20 _
-				Select product
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			gridControl1.DataSource = source.ToList()
-			CreateFilterColumns()
-		End Sub
+        Private source As IQueryable(Of Products)
 
-		Private Sub CreateFilterColumns()
-			filterControl1.FilterColumns.Add(New UnboundFilterColumn("ProductID", "ProductID", GetType(Integer), New RepositoryItemSpinEdit(), FilterColumnClauseClass.Generic))
-			filterControl1.FilterColumns.Add(New UnboundFilterColumn("Quantity", "Quantity", GetType(Integer), New RepositoryItemSpinEdit(), FilterColumnClauseClass.Generic))
-			filterControl1.FilterColumns.Add(New UnboundFilterColumn("Price", "Price", GetType(Decimal), New RepositoryItemSpinEdit(), FilterColumnClauseClass.Generic))
-			filterControl1.FilterColumns.Add(New UnboundFilterColumn("ProductName", "ProductName", GetType(String), New RepositoryItemTextEdit(), FilterColumnClauseClass.String))
-			filterControl1.FilterColumns.Add(New UnboundFilterColumn("Discontinued", "Discontinued", GetType(Boolean), New RepositoryItemCheckEdit(), FilterColumnClauseClass.Generic))
-		End Sub
+        Private Overloads Sub OnLoad(ByVal sender As Object, ByVal e As EventArgs)
+            Dim products = New XPQuery(Of Products)(unitOfWork1)
+            source = From product In products Where product.Quantity >= 20 Select product
+            gridControl1.DataSource = source.ToList()
+            CreateFilterColumns()
+        End Sub
 
-		Private Sub OnUpdateFilter(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
-			gridControl1.BeginUpdate()
-			Try
-				Dim converter As New CriteriaToExpressionConverter()
-				Dim filteredData As IQueryable(Of Products) = TryCast(source.AppendWhere(converter, filterControl1.FilterCriteria), IQueryable(Of Products))
-				gridControl1.DataSource = Nothing
-				gridControl1.DataSource = filteredData.ToList()
-			Finally
-				gridControl1.EndUpdate()
-			End Try
-		End Sub
+        Private Sub CreateFilterColumns()
+            filterControl1.FilterColumns.Add(New UnboundFilterColumn("ProductID", "ProductID", GetType(Integer), New RepositoryItemSpinEdit(), FilterColumnClauseClass.Generic))
+            filterControl1.FilterColumns.Add(New UnboundFilterColumn("Quantity", "Quantity", GetType(Integer), New RepositoryItemSpinEdit(), FilterColumnClauseClass.Generic))
+            filterControl1.FilterColumns.Add(New UnboundFilterColumn("Price", "Price", GetType(Decimal), New RepositoryItemSpinEdit(), FilterColumnClauseClass.Generic))
+            filterControl1.FilterColumns.Add(New UnboundFilterColumn("ProductName", "ProductName", GetType(String), New RepositoryItemTextEdit(), FilterColumnClauseClass.String))
+            filterControl1.FilterColumns.Add(New UnboundFilterColumn("Discontinued", "Discontinued", GetType(Boolean), New RepositoryItemCheckEdit(), FilterColumnClauseClass.Generic))
+        End Sub
 
-		Private Sub OnSaveButtonClick(ByVal sender As Object, ByVal e As EventArgs) Handles btnSave.Click
-			Try
-				unitOfWork1.CommitChanges()
-			Catch exc As Exception
-				MessageBox.Show(exc.ToString())
-				unitOfWork1.ReloadChangedObjects()
-			End Try
-		End Sub
-	End Class
+        Private Sub OnUpdateFilter(ByVal sender As Object, ByVal e As EventArgs)
+            gridControl1.BeginUpdate()
+            Try
+                Dim converter As CriteriaToExpressionConverter = New CriteriaToExpressionConverter()
+                Dim filteredData As IQueryable(Of Products) = TryCast(source.AppendWhere(converter, filterControl1.FilterCriteria), IQueryable(Of Products))
+                gridControl1.DataSource = Nothing
+                gridControl1.DataSource = filteredData.ToList()
+            Finally
+                gridControl1.EndUpdate()
+            End Try
+        End Sub
+
+        Private Sub OnSaveButtonClick(ByVal sender As Object, ByVal e As EventArgs)
+            Try
+                unitOfWork1.CommitChanges()
+            Catch exc As Exception
+                Call MessageBox.Show(exc.ToString())
+                unitOfWork1.ReloadChangedObjects()
+            End Try
+        End Sub
+    End Class
 End Namespace
--- VB/WindowsFormsApplication152/Program.vb
+++ VB/WindowsFormsApplication152/Program.vb
@@ -1,81 +1,69 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows.Forms
 Imports DevExpress.Xpo
 Imports DevExpress.Xpo.DB
 Imports DevExpress.Skins
 
 Namespace DXSample
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Dim connectionString As String = AccessConnectionProvider.GetConnectionString("MyDB.mdb")
-			XpoDefault.DataLayer = XpoDefault.GetDataLayer(connectionString, AutoCreateOption.DatabaseAndSchema)
-			XpoDefault.Session = Nothing
 
-			CreateData()
+    Friend Module Program
 
-			SkinManager.EnableFormSkins()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Dim connectionString As String = AccessConnectionProvider.GetConnectionString("MyDB.mdb")
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer(connectionString, AutoCreateOption.DatabaseAndSchema)
+            XpoDefault.Session = Nothing
+            Call CreateData()
+            Call SkinManager.EnableFormSkins()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.Run(New Form1())
+        End Sub
 
-		Private Shared Sub CreateData()
-			Using uow As New UnitOfWork()
-				If uow.FindObject(Of Products)(Nothing) Is Nothing Then
-					Dim product As New Products(uow)
-					product.Discontinued = False
-					product.Price = 100
-					product.Quantity = 20
-					product.ProductName = "Orange"
-
-					product = New Products(uow)
-					product.Discontinued = True
-					product.Price = 140
-					product.Quantity = 50
-					product.ProductName = "Apple"
-
-					product = New Products(uow)
-					product.Discontinued = False
-					product.Price = 30
-					product.Quantity = 120
-					product.ProductName = "Banana"
-
-					product = New Products(uow)
-					product.Discontinued = True
-					product.Price = 200
-					product.Quantity = 80
-					product.ProductName = "Grape"
-
-					product = New Products(uow)
-					product.Discontinued = False
-					product.Price = 300
-					product.Quantity = 10
-					product.ProductName = "Garnet"
-
-					product = New Products(uow)
-					product.Discontinued = True
-					product.Price = 120
-					product.Quantity = 320
-					product.ProductName = "Mandarin"
-
-					product = New Products(uow)
-					product.Discontinued = False
-					product.Price = 250
-					product.Quantity = 60
-					product.ProductName = "Kiwi"
-
-					uow.CommitChanges()
-				End If
-			End Using
-		End Sub
-	End Class
+        Private Sub CreateData()
+            Using uow As UnitOfWork = New UnitOfWork()
+                If uow.FindObject(Of Products)(Nothing) Is Nothing Then
+                    Dim product As Products = New Products(uow)
+                    product.Discontinued = False
+                    product.Price = 100
+                    product.Quantity = 20
+                    product.ProductName = "Orange"
+                    product = New Products(uow)
+                    product.Discontinued = True
+                    product.Price = 140
+                    product.Quantity = 50
+                    product.ProductName = "Apple"
+                    product = New Products(uow)
+                    product.Discontinued = False
+                    product.Price = 30
+                    product.Quantity = 120
+                    product.ProductName = "Banana"
+                    product = New Products(uow)
+                    product.Discontinued = True
+                    product.Price = 200
+                    product.Quantity = 80
+                    product.ProductName = "Grape"
+                    product = New Products(uow)
+                    product.Discontinued = False
+                    product.Price = 300
+                    product.Quantity = 10
+                    product.ProductName = "Garnet"
+                    product = New Products(uow)
+                    product.Discontinued = True
+                    product.Price = 120
+                    product.Quantity = 320
+                    product.ProductName = "Mandarin"
+                    product = New Products(uow)
+                    product.Discontinued = False
+                    product.Price = 250
+                    product.Quantity = 60
+                    product.ProductName = "Kiwi"
+                    uow.CommitChanges()
+                End If
+            End Using
+        End Sub
+    End Module
 End Namespace
